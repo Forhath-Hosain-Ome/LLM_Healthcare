6b6b91058b3162f56b8bcfa050d4e58438caaa13
Forhath-Hosain-Ome <omehasan3@gmail.com>
2026-01-06 00:32:59 +0600

    analytics models-v.01

---
+++ backend/apps/analytics/models/Cohort_model.py
@@ -0,0 +1,13 @@
+from django.db import models
+from apps.patients.models.Patient_Model import PatientModel
+
+class CohortModel(models.Model):
+    cohort_id = models.CharField(primary_key=True, max_length=255)
+    cohort_name = models.CharField(max_length=100)
+    created_by_user_id = models.ForeignKey(PatientModel, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f'{self.cohort_name} created by {self.created_by_user_id}'
